d298c07fe19c4632fca5f74da2d99660c5b09d2d
unknown <804831771@qq.com>
2015-07-21 22:09:14 +0800

    Silica's Mamori.

--- extensions/SwordArtOnline.lua
+++ extensions/SwordArtOnline.lua
@@ -1,2 +1,69 @@
 --By smwlover at JUL/2015.
 --Sword Art Online Project for QSanguosha.
+module("extensions.swordartonline",package.seeall)
+extension=sgs.Package("swordartonline")
+
+--增加SAO势力
+do
+    require  "lua.config" 
+	local config = config
+	local kingdoms = config.kingdoms
+	table.insert(kingdoms,"sao")
+	config.color_de = "#FF00FF"
+end
+sgs.LoadTranslationTable{
+	["sao"] = "剑",
+	["swordartonline"] = "刀剑神域",
+}
+
+--SAO-104 Silica
+Silica = sgs.General(extension,"Silica","sao","3",false)
+
+--Mamori
+Mamori = sgs.CreateTriggerSkill{
+	name = "LuaMamori",
+	events = {sgs.EventPhaseStart},
+	frequency = sgs.Skill_Frequent,
+	can_trigger = function(self, target)
+		return target ~= nil
+	end,
+	on_trigger = function(self, event, player, data)
+		local room = player:getRoom()
+		if player:getPhase() ~= sgs.Player_Start then
+			return false
+		end
+		local silica = room:findPlayerBySkillName(self:objectName())
+		if not silica then
+			return false
+		end
+		if silica:isWounded() then
+			if room:askForSkillInvoke(silica, self:objectName(), sgs.QVariant("recover")) then
+				local judge = sgs.JudgeStruct()
+				judge.pattern = ".|heart"
+				judge.good = true
+				judge.reason = self:objectName()
+				judge.who = silica
+				room:judge(judge)
+				if judge:isGood() then
+					room:recover(silica, sgs.RecoverStruct(silica))
+				end
+			end
+		end
+		return false
+	end
+}
+Silica:addSkill(Mamori)
+
+sgs.LoadTranslationTable{	
+	["Silica"]="西莉卡",
+	["&Silica"]="西莉卡",
+	["#Silica"]="龙使",
+	["designer:Silica"]="Smwlover",
+	["cv:Silica"]="日高里菜",
+	["illustrator:Silica"]="",
+	["~Silica"]="",
+	
+	["LuaMamori"]="守护",
+	[":LuaMamori"]="<b>（毕娜的守护）</b>一名角色的准备阶段开始时，若你已受伤，你可以进行一次判定，若判定结果为红桃，你回复1点体力。",
+	["LuaMamori:recover"]="你可以发动技能“毕娜的守护”",
+}
